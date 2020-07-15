0c1b71bcd94910a0e9c175fad6f6efabaa739068
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-15 05:19:42 +0300

    feat: Add frontend to the project

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask import Flask, render_template, request, Response, flash
 from database.models import setup_db, Movies, Actors
 
 # Create flask app
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
 
 
 # Connect to the database
@@ -20,6 +22,15 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 #   @access    Public
 @app.route('/', methods=["GET"])
 def index():
+    return render_template('pages/index.html')
+
+
+#   @desc      Return all the movies
+#   @route     GET /movies
+#   @access    Public
+@app.route('/movies', methods=["GET"])
+def getMovies():
+    movies = Movies.query.all()
     return render_template('pages/index.html')
 
 
